814bf5c3908ef1f53d7d0ca78e3957363f783fad
Andres Alba <tensinkusum@hotmail.com>
2023-07-03 18:30:59 -0500

    forced chain

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(DB, {
 
 // Middleware
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/view');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
